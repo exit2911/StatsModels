917fd2cfcda72bd6da6501c82e9961d1b62773a5
Vivian Nguyen <vyho0711@gmail.com>
2019-06-30 22:09:21 -0400

    Update one_way_anova.py

--- one_way_anova.py
+++ one_way_anova.py
@@ -28,3 +28,32 @@ print(stats.f.ppf(q=1-0.05, dfn=2, dfd=6))
 
 #F value is less than F critical value. fail to reject H0
 
+
+
+'''
+
+EXAMPLE # 2
+
+alpha = 0.05
+
+H0: mean sample 0 = mean sample 24 = mean sample 48
+H1: there is at least one difference in aggression score mean
+    
+    
+    
+'''
+#agression scores
+
+zero = [0,4,2] #group deprived of sleep for 0 hrs
+twenty_four = [3,6,6] #group deprived of sleep for 24 hrs
+forty_eight = [6,8,10] #group deprived of sleep for 48 hrs
+
+
+#get F value : stats.f_oneway(array1,array2,array3)
+print(stats.f_oneway(zero,twenty_four,forty_eight))
+
+#get F critical value : stats.f.ppf(q=1-alpha, dfn=dfbetween, dfd=dfwithin)
+print(stats.f.ppf(q=1-0.05, dfn=2, dfd=6))
+
+#F value is greater than F critical value. reject H0
+
