d5f5b06397114c9a65ee74155e008f6550f0b499
Vivian Nguyen <vyho0711@gmail.com>
2019-07-05 23:22:15 -0400

    Create chi_square_nominal.py

---
+++ chi_square_nominal.py
@@ -0,0 +1,28 @@
+
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+#finding critical value
+# Find the critical value for 95% confidence, and df = groups - 1
+
+crit = stats.chi2.ppf(q = 0.95, df = 3)
+
+
+
+
+df = pd.read_csv('/Users/vyho/Documents/bloodtype.csv', sep = ',')
+
+df.head()
+df = df.dropna()
+
+
+
+
+chi = stats.chisquare(f_obs= df['observed'],   # Array of observed counts
+                f_exp= df['expected']) 
+
+print(chi)
+
+#since chi square is greater than critical value, rejecting null hypothesis. there is evidence that distribution of general student population differs for U.S. pop
