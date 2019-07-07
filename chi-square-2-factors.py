5d7ceed93f20997f98ce3752389b955117e3face
Vivian Nguyen <vyho0711@gmail.com>
2019-07-07 13:23:29 -0400

    Update chi-square-2-factors.py

--- chi-square-2-factors.py
+++ chi-square-2-factors.py
@@ -1,7 +1,4 @@
 '''
-
-
-
 2 factor chi square test
 
 initiate an array where elements are observations from different groups
@@ -14,15 +11,15 @@ result (first element in tuple)
 
 df = (number of row - 1)(number of column - 1) 
 
-python will calculate df. no manual calculations
-
-                
 '''
 
 
 from scipy.stats import chi2_contingency
 
 
+crit = stats.chi2.ppf(q = 0.95, df = 2) #critical value for 2 factor chi square test 
+
 obs = np.array([[39, 30, 51], [21, 40, 19]])
 
 chi2_contingency(obs)
+
