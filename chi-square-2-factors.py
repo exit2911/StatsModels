993ebaf5cd84e2e8beeabecacd265e52f9152774
Vivian Nguyen <vyho0711@gmail.com>
2019-07-07 12:54:41 -0400

    Update chi-square-2-factors.py

--- chi-square-2-factors.py
+++ chi-square-2-factors.py
@@ -1,5 +1,28 @@
 '''
 
-df = (number of row - 1)(number of column - 1)
 
+
+2 factor chi square test
+
+initiate an array where elements are observations from different groups
+
+array([group 1],[group 2])
+
+chi2_contingency(array) will return the contigency table (with only expected values) as well as chi square 
+
+result (first element in tuple)
+
+df = (number of row - 1)(number of column - 1) 
+
+python will calculate df. no manual calculations
+
+                
 '''
+
+
+from scipy.stats import chi2_contingency
+
+
+obs = np.array([[39, 30, 51], [21, 40, 19]])
+
+chi2_contingency(obs)
